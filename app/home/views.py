53b5645fdd29ae819b7939b581660d77cfeeca30
Ruslan Bilyshko <ruslan.ua@mail.ru>
2017-04-13 00:02:06 +0300

    Добавление стилей

--- app/home/views.py
+++ app/home/views.py
@@ -8,31 +8,9 @@ from django.contrib.auth import views
 from django.urls import reverse
 
 
-@login_required(login_url="/login")
 def index(request):
-    template_response = views.login(request)
-    return template_response
-
-
-# def login(request):
-#     username = request.POST['username']
-#     password = request.POST['password']
-#     user = auth.authenticate(username=username, password=password)
-#     if user is not None and user.is_active:
-#         # Правильный пароль и пользователь "активен"
-#         auth.login(request, user)
-#         # Перенаправление на "правильную" страницу
-#         return HttpResponseRedirect(reverse("quest:index"))
-#     else:
-#         # Отображение страницы с ошибкой
-#         return render(
-#             request=request,
-#             template_name="home/index.html",
-#             # context={'questions': questions, "quest": quest, "form": form, "data": rdata}
-#         )
-#
-#
-# def logout(request):
-#     auth.logout(request)
-#     # Перенаправление на страницу.
-#     return HttpResponseRedirect("/account/loggedout/")
+    if not request.user.is_authenticated:
+        template_response = views.login(request)
+        return template_response
+    else:
+        return HttpResponseRedirect(reverse('quest:index'))
